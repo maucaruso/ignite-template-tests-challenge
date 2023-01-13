7dc00442f8bb018ddda20de881b33aa8880e2dcf
maucaruso <carusojr@outlook.com>
2023-01-13 08:02:28 -0300

    test: Show User Profile

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { AuthenticateUserUseCase } from "../authenticateUser/AuthenticateUserUseCase";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let showUserProfileUseCase: ShowUserProfileUseCase;
+
+describe("Show User Profile", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+    showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository);
+  });
+
+  it ("Should be able to show a user profile by user id", async () => {
+    const user = {
+      "name": "username",
+      "email": "username@test.com",
+      "password": "password"
+    }
+
+    await createUserUseCase.execute(user);
+
+    const createdUser = await inMemoryUsersRepository.findByEmail(user.email);
+
+    if (createdUser?.id) {
+      const response = await showUserProfileUseCase.execute(createdUser.id);
+      console.log(response);
+      expect(response.id).toBe(createdUser.id);
+    }
+  });
+});
